b2f0278956804fc7e9d55d710d64b6198080f029
munanhui <slimelimeline@gmail.com>
2025-03-11 22:37:42 +0900

    --disable-gpu

--- app.py
+++ app.py
@@ -258,6 +258,9 @@ def index():
                 options.add_argument("--headless")
                 options.add_argument("--no-sandbox")
                 options.add_argument("--disable-dev-shm-usage")
+                options.add_argument("--disable-gpu")
+                options.add_argument("--disable-software-rasterizer")
+                options.add_argument("--disable-features=VizDisplayCompositor")
 
                 driver = webdriver.Chrome(service=service, options=options)
 
